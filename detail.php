3a3e17919e3af3aee84bf5e82752eda458df3d5e
Zidan Abraham <zidangit07@gmail.com>
2022-03-17 17:45:27 +0800

    koneksi dan menampilkan data

---
+++ detail.php
@@ -0,0 +1,35 @@
+<?php 
+require 'functions.php';
+
+$id = $_GET['id'];
+$anime = query("SELECT * FROM anime WHERE id = $id");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Anime Details</title>
+   <style>
+      html {
+         text-align: center;
+         font-family: Arial, Helvetica, sans-serif;
+      }
+   </style>
+</head>
+<body>
+   
+      <h1><?= $anime['Title']; ?></h1>
+
+      <p><img src="img/<?= $anime['Poster']; ?>" alt=""></p>
+      <p>Studio : <?= $anime['Studio']; ?></p>
+      <p>Source : <?= $anime['Source']; ?></p>
+      <p>Premiered : <?= $anime['Premiered']; ?></p>
+      <p>MC : <?= $anime['MC']; ?></p>
+      <p><a href="">Edit</a> | <a href="">Delete</a></p>
+      <p><a href="latihan3.php">Back to Anime List</a></p>
+
+</body>
+</html>
---
+++ latihan2.php
@@ -0,0 +1,58 @@
+<?php 
+require 'functions.php';
+$anime = query("SELECT * FROM anime");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Anime List</title>
+   <style>
+      html {
+         font-family: Arial, Helvetica, sans-serif;
+         text-align: center;
+      }
+
+      table {
+         width: 100%;
+      }
+   </style>
+</head>
+<body>
+   
+   <h1>Anime List</h1>
+
+   <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+         <th>No</th>
+         <th>Poster</th>
+         <th>Title</th>
+         <th>Studio</th>
+         <th>Source</th>
+         <th>Premiered</th>
+         <th>MC</th>
+         <th>Action</th>
+      </tr>
+
+      <?php $i = 1; ?>
+      <?php foreach($anime as $nimek) : ?>
+      <tr>
+         <td><?= $i++; ?></td>
+         <td><img src="img/<?= $nimek['Poster']; ?>" alt="Poster"></td>
+         <td><?= $nimek['Title']; ?></td>
+         <td><?= $nimek['Studio']; ?></td>
+         <td><?= $nimek['Source']; ?></td>
+         <td><?= $nimek['Premiered']; ?></td>
+         <td><?= $nimek['MC']; ?></td>
+         <td>
+            <a href="">Edit</a> | <a href="">Delete</a>
+         </td>
+      </tr>
+      <?php endforeach; ?>
+   </table>
+
+</body>
+</html>
